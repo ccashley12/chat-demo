80ddc18dec1dfc18ddf602543d22bea22d926323
ccashley12 <ccashley12@gmail.com>
2025-02-16 12:05:02 -0500

    5.4 imported asyncstorage, netinfo, and offline features

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import Chat from './components/Chat';
 import Start from './components/Start';
+import { LogBox, Alert } from 'react-native';
+
+//Import navigator
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
+//Import Firebase
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
 
-import { LogBox } from 'react-native';
+//Import Async Storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//Import NetInfo for internet connection
+import { useNetInfo } from '@react-native-community/netinfo';
+
 LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 const App = () => {
@@ -29,6 +39,32 @@ const App = () => {
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
 
+  //Defines network connectivity status
+  const connectionStatus = useNetInfo();
+
+  const[isConnected, setIsConnected] = useState(connectionStatus.isConnected ?? false);
+
+  //Alert if network connection is lost and regulate attempts to reconnect to Firebase db
+  useEffect(() => {
+    if (connectionStatus.isConnected !== null && connectionStatus.isConnected !== isConnected) {
+      setIsConnected(connectionStatus.isConnected);
+
+      const updateFirestoneConnection = async () => {
+          try {
+                if (connectionStatus.isConnected === false) {
+                  Alert.alert('You are offline. You can read messages, but cannot send messages.');
+                  await disableNetwork(db);
+                } else {
+                  await enableNetwork(db);
+                }
+          } catch (error) {
+            console.error('Error managing Firestone network:', error);
+          }
+      };
+      updateFirestoneConnection();
+    }
+  }, [connectionStatus.isConnected]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -39,7 +75,11 @@ const App = () => {
           component={Start}
         />
         <Stack.Screen name="Chat">
-          {props => <Chat db={db} {...props} />}
+          {props => 
+            <Chat 
+              isConnected={connectionStatus.isConnected} 
+              db={db} 
+              {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
